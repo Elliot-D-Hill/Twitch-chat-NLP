fd74c47b32fc789d9d9464d8a9994ad748962b1d
stillahill <elliot.douglas.hill@gmail.com>
2021-04-14 13:29:28 -0500

    add Dockerfile and docker-compose.ylm

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
-#!/usr/bin/python3
 
+from db import database
+from twitch_nlp import chatbot
 import config
-import db.database
-import app.chatbot
 
-username = 'maieutikos'
-token = config.OAUTH_TOKEN
-channels = ['maieutikos']
-# comment batch size before database insertion
-max_batch_size = 1  # hint: larger batch sizes are more efficient
 
-# initialize PostgreSQL database
-twitch_db = db.database.Database(
-    filename='db/database.ini', section='postgresql')
+def main():
+    username = 'deviousdata'
+    token = config.OAUTH_TOKEN
+    channels = ['deviousdata']
+    # comment batch size before database insertion
+    max_batch_size = 1  # hint: larger batch sizes are more efficient
 
-# initialize chat bot
-bot = app.chatbot.ChatBot(twitch_db, username, token, channels)
+    # initialize PostgreSQL database
+    twitch_database = database.Database(
+        filename='db/database.ini', section='postgresql')
 
-# create tables in database, if they don't already exist
-twitch_db.create_tables(filename='db/twitch_chat_tables.sql')
+    # initialize chat bot
+    bot = chatbot.ChatBot(twitch_database, username, token, channels)
 
-# connect to twitch chat and log user messages
-bot.connect(max_batch_size)
+    # create tables in database, if they don't already exist
+    twitch_database.create_tables(filename='db/twitch_chat_tables.sql')
+
+    # connect to twitch chat and log user messages
+    bot.connect(max_batch_size)
+
+
+if __name__ == "__main__":
+    main()
